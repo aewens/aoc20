a3f216fddaaab4f880012bd42b115d4a77d5e90d
aewens <email@aewens.com>
2020-12-01 17:10:30 +0000

    Day 1, Part 1 & 2

---
+++ pkg/solutions/aoc1.go
@@ -0,0 +1,60 @@
+package solutions
+
+import (
+	"github.com/aewens/aoc20/pkg/shared"
+)
+
+func init() {
+	Map[1] = Solution1
+}
+
+func product(xs []int) int {
+	result := 1
+	for _, x := range xs {
+		result = result * x
+	}
+	return result
+}
+
+func SearchDouble2020(transactions []int) ([]int){
+	for t, transaction := range transactions {
+		for tt, tTransaction := range transactions {
+			if t == tt {
+				continue
+			}
+
+			if transaction + tTransaction == 2020 {
+				return []int{transaction, tTransaction}
+			}
+		}
+	}
+	return []int{-1,-1}
+}
+
+func SearchTriple2020(transactions []int) ([]int){
+	for t, transaction := range transactions {
+		for tt, tTransaction := range transactions {
+			for ttt, ttTransaction := range transactions {
+				if t == tt || t == ttt || tt == ttt {
+					continue
+				}
+
+				if transaction + tTransaction + ttTransaction == 2020 {
+					return []int{transaction, tTransaction, ttTransaction}
+				}
+			}
+		}
+	}
+	return []int{-1, -1, -1}
+}
+
+func Solution1(lines chan string) {
+	transactions := []int{}
+	for line := range lines {
+		transaction := shared.StringToInt(line)
+		transactions = append(transactions, transaction)
+	}
+
+	Display(1, product(SearchDouble2020(transactions)))
+	Display(2, product(SearchTriple2020(transactions)))
+}
---
+++ pkg/solutions/solutions_test.go
@@ -0,0 +1,12 @@
+package solutions
+
+//import (
+//	"testing"
+//)
+
+//func Cleanup(t *testing.T) {
+//	r := recover()
+//	if r != nil {
+//		t.Fatal(r)
+//	}
+//}
